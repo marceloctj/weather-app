c57a303c43322254ff12e1aba2549885dcea0513
Marcelo Travasso <marcelo.travasso@pebmed.com.br>
2022-07-29 20:26:36 -0300

    chore: ensure env configuration

---
+++ src/types/env.d.ts
@@ -0,0 +1,6 @@
+declare module '@env' {
+  export const OPENWEATHERMAP_API_GEOCODING: string;
+  export const OPENWEATHERMAP_API_WEATHER: string;
+  export const OPENWEATHERMAP_KEY: string;
+  export const OPENWEATHERMAP_ICONS: string;
+}
