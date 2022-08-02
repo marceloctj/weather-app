bf2ea2e3eaaa72b1eb0a07e40f09a0cd07545732
Marcelo Travasso <marcelo.travasso@pebmed.com.br>
2022-08-02 19:52:31 -0300

    feat: create global Text component

---
+++ src/presentation/components/Text/Text.tsx
@@ -0,0 +1,46 @@
+import React, { PropsWithChildren } from 'react';
+import { TextProps } from 'react-native';
+import { DefaultTheme } from 'styled-components';
+import styled from 'styled-components/native';
+
+type Props = PropsWithChildren<{
+  font?: keyof DefaultTheme['fonts'];
+  variant?: keyof DefaultTheme['palette'];
+  scale?:
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'h4'
+    | 'h5'
+    | 'h6'
+    | 'body1'
+    | 'body2'
+    | 'button';
+  useContrast?: boolean;
+}> &
+  TextProps;
+
+const scales: { [key in Props['scale']]: number } = {
+  h1: 96,
+  h2: 60,
+  h3: 48,
+  h4: 34,
+  h5: 24,
+  h6: 20,
+  body1: 16,
+  body2: 14,
+  button: 14,
+};
+
+const StyledText = styled.Text<Props>`
+  font-family: ${({ font, theme }) => theme.fonts[font] || theme.fonts.regular};
+  color: ${({ variant, theme, useContrast }) =>
+    (theme.palette[variant] || theme.palette.primary)?.[
+      useContrast ? 'contrastText' : 'main'
+    ]};
+  font-size: ${({ scale }) => scales[scale || 'body1']}px;
+`;
+
+export const Text: React.FC<Props> = ({ children, ...textProps }) => (
+  <StyledText {...textProps}>{children}</StyledText>
+);
