e2c0a8930167fbe0a2a808e6866255191caba50f
Marcelo Travasso <marcelo.travasso@pebmed.com.br>
2022-08-08 13:49:50 -0300

    chore: add test utils

---
+++ src/presentation/config/test-utils.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+
+import { ThemeProvider } from 'styled-components/native';
+import { theme } from '@presentation/config/theme';
+
+const Provider: React.FC<any> = ({ children }) => {
+  return <ThemeProvider theme={theme.default}>{children}</ThemeProvider>;
+};
+
+const renderWithTheme = (component: React.ReactElement<any>) => {
+  return render(component, { wrapper: Provider });
+};
+
+export * from '@testing-library/react-native';
+
+export { renderWithTheme as themedRender };
