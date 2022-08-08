d5689dd5f610f26586e512c9716ac2c96c0474ed
Marcelo Travasso <marcelo.travasso@pebmed.com.br>
2022-08-08 16:40:38 -0300

    chore: ignore styles in jest coverage

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,5 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native|react-navigation|@react-native-community|@react-navigation)',
   ],
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/styles.ts'],
 };
