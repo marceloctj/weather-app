e8ec0aa9b5df88bae524bb2be2744d3829ab61d6
Marcelo Travasso <marcelo.travasso@pebmed.com.br>
2022-08-08 18:01:48 -0300

    test: ensure weather hourly panel component

--- src/presentation/screens/main/Home/components/WeatherHourlyPanel/WeatherHourlyPanel.tsx
+++ src/presentation/screens/main/Home/components/WeatherHourlyPanel/WeatherHourlyPanel.tsx
@@ -3,24 +3,19 @@ import React from 'react';
 import { Container, Item, TextHour, TextTemp, MiniImage } from './styles';
 
 import { useAppSelector } from '@presentation/store/hooks';
+import { formatToHourString } from '@presentation/utils/time';
 
 export const WeatherHourlyPanel: React.FC = () => {
   const hourlyForecast = useAppSelector(state => state.home.weather?.hourly);
-
-  const makeHourString = (date: Date) => {
-    const hour = date.getHours();
-    const period = hour > 12 ? 'PM' : 'AM';
-    return `${hour.toString().padStart(2, '0')} ${period}`;
-  };
 
   return (
     <Container>
       {hourlyForecast &&
         hourlyForecast.map((item, index: number) => (
-          <Item key={index}>
-            <TextHour>{makeHourString(item.datetime)}</TextHour>
+          <Item testID="weather-hourly-item" key={index}>
+            <TextHour>{formatToHourString(item.datetime)}</TextHour>
             <MiniImage source={item.icon} />
-            <TextTemp>{item.temp?.main}</TextTemp>
+            <TextTemp>{item.temp?.main}º</TextTemp>
           </Item>
         ))}
     </Container>
