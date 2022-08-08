fe1eb15a5dde7da7a389d2a2bbd8761c29135139
Marcelo Travasso <marcelo.travasso@pebmed.com.br>
2022-08-08 00:16:15 -0300

    chore: install testing libs

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ const { defaults } = require('jest-config');
 
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native|react-navigation|@react-native-community|@react-navigation)',
