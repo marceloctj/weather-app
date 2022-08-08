451d24f103b787ea1b34b3fa2769365c168e60bb
Marcelo Travasso <marcelo.travasso@pebmed.com.br>
2022-08-08 00:35:00 -0300

    test: fix jest config collection coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 const { defaults } = require('jest-config');
 
+console.log(defaults.moduleFileExtensions);
 module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
@@ -7,4 +8,5 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native|react-navigation|@react-native-community|@react-navigation)',
   ],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
 };
